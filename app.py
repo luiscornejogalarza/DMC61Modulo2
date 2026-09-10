b52d4877dd9a74b69300ea406f86b7bd8e0d8b25
luiscornejogalarza <luiscornejogalarza@gmail.com>
2026-09-09 21:39:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+#Librerias
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import io
+
+# Titulo
+st.set_page_config(page_title="EDA - Salud Mental Adolescente", layout="wide")
